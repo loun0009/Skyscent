a9b63c42c7aed2451abfe52c0085885bb86243be
Enzo Loungoundji <enzo.loungoundji@etudiant.univ-reims.fr>
2026-03-09 14:49:25 +0100

    chore: setup Jest 29 + jest-expo with babel config

---
+++ jest.setup.ts
@@ -0,0 +1,17 @@
+jest.mock("@react-native-async-storage/async-storage", () => ({
+  getItem: jest.fn(() => Promise.resolve(null)),
+  setItem: jest.fn(() => Promise.resolve()),
+  removeItem: jest.fn(() => Promise.resolve()),
+  clear: jest.fn(() => Promise.resolve()),
+  getAllKeys: jest.fn(() => Promise.resolve([])),
+}));
+
+jest.mock("expo-router", () => ({
+  useRouter: () => ({
+    push: jest.fn(),
+    back: jest.fn(),
+    replace: jest.fn(),
+  }),
+  useLocalSearchParams: () => ({ id: "1" }),
+  useSegments: () => [],
+}));
